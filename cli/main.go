49fc7d5297633b499c2fb5c581281325e71324b4
Louis <tagnacht2@gmail.com>
2026-03-21 08:43:30 +0100

    wip: work on proper cli

--- cli/main.go
+++ cli/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -16,13 +17,47 @@ const (
 	ARCH_armhf   = "armhf"
 )
 
+func checkArch(arch *string) error {
+	switch *arch {
+	case ARCH_x86_64, ARCH_aarch64, ARCH_i686, ARCH_armhf:
+		fmt.Printf("[flags] System architecture %s is known\n", *arch)
+		return nil
+	}
+
+	return fmt.Errorf("[flags] System architecture %s is unknown", *arch)
+}
+
 func main() {
 	location, err := os.Getwd()
 	ait.Check(err)
+
+	arch := flag.String("arch", "x86_64", "System Architecture on which the AppImage should run. Valid values are: x86_64, aarch64, i686, armhf")
+	runtimePath := flag.String("runtime-file", "", "(Optional) Path of AppImage runtime which BECOMES the AppImage")
+	privKeyPath := flag.String("sign-key", "", "(Optional) Path of PGP private key file to sign the AppImage")
+	passphrase := flag.String("passphrase", "", "(Optional) Passphrase of encrypted PGP key file. Only use if encrypted.")
+	flag.Parse()
+
+	if err := checkArch(arch); err != nil {
+		if *runtimePath == "" {
+			ait.Check(fmt.Errorf("Unknown system architecture supplied. Supply -runtime-file or choose from (x86_64, aarch64, i686, armhf)"))
+		}
+	}
+
+	if *privKeyPath != "" || *passphrase != "" {
+		fmt.Println("[flags] Warning: Code signing is not yet implemented")
+	}
+
+	cliArgs := flag.Args()
+	if len(cliArgs) <= 0 {
+		ait.Check(fmt.Errorf("[flags] No AppDir supplied"))
+	}
+
+	fmt.Printf("Args: %s\n", cliArgs[0])
+
 	outFileName := filepath.Join(location, "test_fs.squashfs")
 
 	os.Remove(outFileName)
-	ait.CreateSquashFSFromFolder(filepath.Join(location, "test_fs"), outFileName)
+	ait.CreateSquashFSFromFolder(filepath.Join(location, "test.AppDir"), outFileName)
 	fileName := ait.DownloadAppImageEngine(ARCH_x86_64)
 	ait.AppendToFile(outFileName, fileName)
 	hash := ait.CalculateMD5(fileName)
